a1fe039c9d7777ca31610c1cdfaa07d8d60fe980
minhnbnt <minhnbnt@gmail.com>
2024-07-30 07:32:55 +0700

    feat: switch to styled notification

---
+++ src/lib/layouts/notification/element.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { Icon, XMark } from 'svelte-hero-icons';
+	import { fly } from 'svelte/transition';
+
+	export let notification;
+	export let onRemove;
+
+	const { text, type } = notification;
+
+	function getBgColor() {
+		if (type === 'error') {
+			return 'bg-red-600 text-white';
+		}
+
+		if (type === 'warning') {
+			return 'bg-yellow-600 text-white';
+		}
+
+		if (type === 'success') {
+			return 'bg-green-600 text-white';
+		}
+
+		return 'bg-white text-black border';
+	}
+
+	function getBorderColor() {
+		const types = ['error', 'warning', 'success'];
+		if (types.some((s) => type === s)) {
+			return 'border-white';
+		}
+
+		return 'border-black';
+	}
+</script>
+
+<div class={`${getBgColor()} notification`} transition:fly|global={{ duration: 200, y: '50%' }}>
+	<p class={`${getBorderColor()} mr-2 min-w-fit grow border-r pr-3`}>{text}</p>
+	<button on:click={onRemove} class={`${getBorderColor()}`}>
+		<Icon src={XMark} class="size-6" />
+	</button>
+</div>
+
+<style>
+	.notification {
+		@apply m-3 flex min-w-[180px] items-center justify-between rounded-lg p-3 shadow;
+	}
+</style>
--- src/lib/layouts/notification/main.svelte
+++ src/lib/layouts/notification/main.svelte
@@ -1,10 +1,11 @@
 <script>
 	import Notifications from 'svelte-notifications';
 
+	import Element from './element.svelte';
 	import Wrapper from './wrapper.svelte';
 </script>
 
-<Notifications>
+<Notifications item={Element} zIndex={2000}>
 	<Wrapper>
 		<slot />
 	</Wrapper>
