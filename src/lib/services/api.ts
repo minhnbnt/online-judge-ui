3c6925ea9115ad0b7eb5f08b211d2724c398b5cd
minhnbnt <minhnbnt@gmail.com>
2025-04-27 19:06:42 +0700

    fix: fixed handle error when using msgpack

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,24 +1,33 @@
-import axios from 'axios';
-import { type AxiosResponse } from 'axios';
+import axios, { type AxiosResponse, type AxiosError } from 'axios';
 import { decode } from '@msgpack/msgpack';
 
 import { PUBLIC_API_BASE_URL } from '$env/static/public';
 
+const MSGPACK = 'application/msgpack';
+
 export const instance = axios.create({
 	baseURL: PUBLIC_API_BASE_URL,
-	headers: { Accept: 'application/msgpack' },
+	headers: { Accept: MSGPACK },
 	responseType: 'arraybuffer'
 });
 
-async function decodeResponse(response: AxiosResponse) {
-	if (response.data) {
+function decodeResponse(response: AxiosResponse) {
+	const contentType = response.headers['content-type'];
+	const needDecodeResponse = response.data && contentType === MSGPACK;
+
+	if (needDecodeResponse) {
 		response.data = decode(new Uint8Array(response.data));
 	}
 
 	return response;
 }
 
-instance.interceptors.response.use(
-	decodeResponse,
-	decodeResponse //
-);
+function onError(error: AxiosError) {
+	if (error.response !== undefined) {
+		error.response = decodeResponse(error.response);
+	}
+
+	return Promise.reject(error);
+}
+
+instance.interceptors.response.use(decodeResponse, onError);
