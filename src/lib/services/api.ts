104d1922666423187e2a53eb100804053f2d4c3b
minhnbnt <minhnbnt@gmail.com>
2025-05-16 10:34:42 +0700

    feat: make msgpack avaliable in production only

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,12 +1,32 @@
-import axios, { type AxiosRequestHeaders, type AxiosResponseHeaders } from 'axios';
+import axios, {
+	type AxiosRequestHeaders,
+	type AxiosResponseHeaders,
+	type CreateAxiosDefaults
+} from 'axios';
 import { decode, encode } from '@msgpack/msgpack';
 
 import { PUBLIC_API_BASE_URL } from '$env/static/public';
+import { dev } from '$app/environment';
 
 const MSGPACK_CONTENT_TYPE = 'application/msgpack';
 
+let textEncoder: TextDecoder | undefined;
+
+function decodeResponse(data: ArrayBuffer, headers: AxiosResponseHeaders) {
+	if (headers['content-type'] === MSGPACK_CONTENT_TYPE) {
+		return decode(data);
+	}
+
+	if (textEncoder === undefined) {
+		textEncoder = new TextDecoder('utf-8');
+	}
+
+	return JSON.parse(textEncoder.decode(data));
+}
+
 function encodeRequest(data: unknown, headers: AxiosRequestHeaders) {
-	if (!(data instanceof FormData)) {
+	const isFromData = data instanceof FormData;
+	if (!isFromData) {
 		headers['Content-Type'] = MSGPACK_CONTENT_TYPE;
 		data = encode(data);
 	}
@@ -14,18 +34,18 @@ function encodeRequest(data: unknown, headers: AxiosRequestHeaders) {
 	return data;
 }
 
-function decodeResponse(data: ArrayBuffer, headers: AxiosResponseHeaders) {
-	if (headers['content-type'] === MSGPACK_CONTENT_TYPE) {
-		return decode(data);
-	}
+let config: CreateAxiosDefaults = {
+	baseURL: PUBLIC_API_BASE_URL
+};
 
-	return data;
+if (!dev) {
+	config = {
+		headers: { Accept: MSGPACK_CONTENT_TYPE },
+		transformResponse: [decodeResponse],
+		transformRequest: [encodeRequest],
+		responseType: 'arraybuffer',
+		...config
+	};
 }
 
-export const instance = axios.create({
-	headers: { Accept: MSGPACK_CONTENT_TYPE },
-	transformResponse: [decodeResponse],
-	transformRequest: [encodeRequest],
-	baseURL: PUBLIC_API_BASE_URL,
-	responseType: 'arraybuffer'
-});
+export const instance = axios.create(config);
